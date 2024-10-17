7750cfdb076a629f9c70d40408159f5c2ed312e9
Brett Kuskie <fullaxx@gmail.com>
2024-10-17 20:48:14 +0000

    Update to lazygit-0.44.1

--- scripts/add_lazygit.sh
+++ scripts/add_lazygit.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-LGVERS="0.40.2"
+LGVERS="0.44.1"
 LGURL="https://github.com/jesseduffield/lazygit/releases/download/v${LGVERS}/lazygit_${LGVERS}_Linux_x86_64.tar.gz"
+LGFILE="lazygit.tar.gz"
 
 mkdir -p /usr/share/doc/lazygit-${LGVERS}
 cd /usr/share/doc/lazygit-${LGVERS}
-wget ${LGURL} -O lazygit.tar.gz
-tar xvf lazygit.tar.gz
-rm lazygit.tar.gz
+wget ${LGURL} -O ${LGFILE}
+tar xvf ${LGFILE}
+rm ${LGFILE}
 mv lazygit /usr/bin/
