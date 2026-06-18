cc68f980119433fb723c762ee00e0ea0a3e163a9
Brett Kuskie <fullaxx@gmail.com>
2026-06-18 18:27:13 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+set -e
+
 #for BASEIMAGE in `grep '^FROM' Dockerfile | awk '{print $2}'`; do
 #  docker pull ${BASEIMAGE}
 #done
 
 git pull
+
 docker build -t "ghcr.io/fullaxx/brettdev" .
+
 docker build -t "ghcr.io/fullaxx/brettdev-full" -f Dockerfile.full .
